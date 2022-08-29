d6f5334c426b4c001b998493201751e419b6e867
bafkit <konyrevpv@mail.ru>
2022-08-29 23:31:55 +0300

    Practical task four

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -3,8 +3,14 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Anim;
 import com.mygdx.game.Main;
@@ -13,6 +19,11 @@ public class GameScreen implements Screen {
     private final Main game;
     private final SpriteBatch batch;
     Anim animation;
+    private OrthographicCamera camera;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private Vector2 mapPosition;
+
     boolean dir;
     float x;
     float y;
@@ -23,6 +34,15 @@ public class GameScreen implements Screen {
         batch = new SpriteBatch();
         //animation = new Anim("mesomorph.png", 9, 6, Animation.PlayMode.LOOP, 7,8,9);
         animation = new Anim("atlas/unnamed.atlas", Animation.PlayMode.LOOP);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+
+        map = new TmxMapLoader().load("map/Tiles_map_project.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+        mapPosition = new Vector2();
+
+        map.getLayers().get("Объекты").getObjects().getByType(RectangleMapObject.class);
+        RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("Объекты").getObjects().get("Камера");
     }
 
     @Override
@@ -32,6 +52,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        camera.update();
         ScreenUtils.clear(0, 0.5f, 0.5f, 1);
 
         animation.setTime(Gdx.graphics.getDeltaTime());
@@ -61,9 +82,13 @@ public class GameScreen implements Screen {
             dir = false;
         }
 
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(animation.getFrame(), x, y);
         batch.end();
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
             dispose();
@@ -73,7 +98,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
     }
 
     @Override
